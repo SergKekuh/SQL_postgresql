d906b4b1c70121d2b504818372af493985fd4062
SergKekuh <serg.kekuh@gmail.com>
2025-08-26 11:43:06 +0000

    Update version to v1.0.0-33-g4de5c15 and log timestamps in LastTest.log

--- cod_Migration/version/version.hpp
+++ cod_Migration/version/version.hpp
@@ -1,4 +1,4 @@
 #pragma once
-#define APP_VERSION "v1.0.0-30-g0bc850a"
-#define GIT_COMMIT "0bc850a"
+#define APP_VERSION "v1.0.0-33-g4de5c15"
+#define GIT_COMMIT "4de5c15"
 #define GIT_BRANCH "main"
